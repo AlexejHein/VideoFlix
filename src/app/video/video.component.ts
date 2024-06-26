968c1637ca52aaf69fbd89ad40ec65b6bd5dc41f
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2024-06-26 10:58:28 +0200

    first commit

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,34 +1,56 @@
 import { Component } from '@angular/core';
-import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardImage} from "@angular/material/card";
-import {MatButton} from "@angular/material/button";
-import {NgOptimizedImage} from "@angular/common";
-import {MatCardModule} from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { NgForOf, NgOptimizedImage } from "@angular/common";
+
+
+
 
 @Component({
   selector: 'app-video',
   standalone: true,
   imports: [
-    MatCard,
-    MatCardHeader,
-    MatCardImage,
-    MatCardContent,
-    MatCardActions,
-    MatButton,
-    NgOptimizedImage,
     MatCardModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    NgForOf,
+    NgOptimizedImage,
   ],
   templateUrl: './video.component.html',
-  styleUrl: './video.component.scss'
+  styleUrls: ['./video.component.scss']
 })
 export class VideoComponent {
-  video: {title: string, year: number, description: string} = {
-    title: 'Video Title 1',
-    year: 2024,
-    description: 'Description 1'
-  };
 
+  videos = [
+    {title: 'Video Title 1', year: 2024, description: 'Description 1'},
+    {title: 'Video Title 2', year: 2025, description: 'Description 2'},
+    {title: 'Video Title 3', year: 2026, description: 'Description 3'},
+    {title: 'Video Title 4', year: 2027, description: 'Description 4'},
+    {title: 'Video Title 5', year: 2028, description: 'Description 5'},
+    {title: 'Video Title 6', year: 2029, description: 'Description 6'},
+    {title: 'Video Title 7', year: 2030, description: 'Description 7'},
+    {title: 'Video Title 8', year: 2031, description: 'Description 8'}
+  ];
+
+
+  currentIndex = 0;
+  visibleCount = 3;
+
+  getVisibleVideos() {
+    const visibleVideos = [];
+    for (let i = 0; i < this.visibleCount; i++) {
+      visibleVideos.push(this.videos[(this.currentIndex + i) % this.videos.length]);
+    }
+    return visibleVideos;
+  }
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.videos.length;
+  }
+
+  prev() {
+    this.currentIndex = (this.currentIndex - 1 + this.videos.length) % this.videos.length;
+  }
 }
+
